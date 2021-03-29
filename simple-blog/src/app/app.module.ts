58824c4697990c8cafda906a9a85adf3f28558e8
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-03-29 15:35:24 +0200

    register component in module

--- simple-blog/src/app/app.module.ts
+++ simple-blog/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {PostItemComponent} from "./post-item/post-item.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BlogComponent
+    BlogComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
