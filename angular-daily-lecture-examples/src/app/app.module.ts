96ceadd26f331a1cf223dcec978807598280be13
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-04-06 18:23:10 +0200

    day8 - example 2 (service  lvl error handling)

--- angular-daily-lecture-examples/src/app/app.module.ts
+++ angular-daily-lecture-examples/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ErrorHandlingLvl1ComponentModule} from "../modules/day8-error-handling/error-handling-lvl1-component/error-handling-lvl1-component.module";
+import {ErrorHandlingLvl2ServiceModule} from "../modules/day8-error-handling/error-handling-lvl2-service/error-handling-lvl2-service.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {ErrorHandlingLvl1ComponentModule} from "../modules/day8-error-handling/e
   ],
   imports: [
     BrowserModule,
-    ErrorHandlingLvl1ComponentModule // day8
+    ErrorHandlingLvl1ComponentModule, // day8
+    ErrorHandlingLvl2ServiceModule // day8
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-daily-lecture-examples/src/modules/day8-error-handling/error-handling-lvl2-service/data.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {Observable, throwError} from "rxjs";
+import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {catchError, retry} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  static  readonly API_URL = `https://jsonplaceholder.typicode.com/posts`;
+
+  constructor(private http: HttpClient) {}
+
+  // get all Users
+  getPosts(limit = 3): Observable<Post[] | string>  {
+    return this.http.get<Post[]>(DataService.API_URL, {
+      params: new HttpParams().set('_limit', limit.toString()) // set limit query param
+    }).pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error: HttpErrorResponse): Observable<string> {
+
+    let errorMessage = '';
+
+    if (error.error instanceof ProgressEvent) {
+      // client-side error
+      errorMessage = `Client-side error: ${error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `server-side error:
+      Error Code: ${error.status}
+      Message: ${error.message}`;
+    }
+
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+}
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
