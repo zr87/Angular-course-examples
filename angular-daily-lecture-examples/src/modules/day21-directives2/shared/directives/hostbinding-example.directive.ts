c3e27b6e3e7869159c4cb46a0acb580799a37fa3
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-05-03 17:55:28 +0200

    day21 directives II. complete

--- angular-daily-lecture-examples/src/modules/day21-directives2/shared/directives/hostbinding-example.directive.ts
+++ angular-daily-lecture-examples/src/modules/day21-directives2/shared/directives/hostbinding-example.directive.ts
@@ -9,6 +9,7 @@ export class HostbindingExampleDirective {
   @HostBinding('style.color') color: string | undefined;
 
   constructor(private el: ElementRef) {
+    // only for demo, modify DOM rather after view has initialized & use ngAfterVirwInit for that!
     this.el.nativeElement.style.backgroundColor = 'lightgreen';
 
     this.border = '1px solid green';
